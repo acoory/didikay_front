68480a84422f0e7f0261347b46330c13aafda22e
Cory Anthony <anthony.cory@epitech.eu>
2025-03-21 20:14:53 +0100

    config .env

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,8 +3,9 @@ import axios from "axios";
 export default class Api {
   public instance: any;
   constructor() {
+    const baseURL = import.meta.env.NODE_ENV === "development" ? import.meta.env.VITE_API_URL_DEV : import.meta.env.VITE_API_URL_PROD;
     this.instance = axios.create({
-      baseURL: `${import.meta.env.VITE_API_URL}`,
+      baseURL: `${baseURL}`,
       withCredentials: true,
     });
   }
